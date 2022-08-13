73695fd7178a5540467526f8c85a50bf0d7a2164
mh60049 <44577580+mh60049@users.noreply.github.com>
2022-08-13 15:22:36 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const Datastore = require('nedb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT  || 3000;
+
+app.listen(port, () => {
+ console.log(`Starting server at ${port}`);
+});
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.get('/api', (request, response) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+  response.json(data);
+  });
+});
+
+app.post('/api', (request, response) => {
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json({
+    status: 'success',
+    mood: data.mood,
+    latitude: data.lat,
+    longitude: data.lon,
+    timestamp: data.timestamp
+  })
+});
